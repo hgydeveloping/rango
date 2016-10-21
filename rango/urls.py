411697960299ae353c18cf756ef150a10c3ecb20
Gyula <hgydeveloping@gmail.com>
2016-10-21 15:05:02 +0200

    New template and JavaScript chart

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from rango import views
 
+
 urlpatterns = [
     url(r'^$',views.index, name='index'),
+    url(r'^test1/',views.test1, name='test1'),
     url(r'^about/',views.about, name='about'),
+
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+import getpass
 
 def index(request):
     context_dict = {'boldmessage': "Hello World!"}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {'myname': getpass.getuser()}
+    return render(request, 'rango/about.html', context=context_dict)
+
+def test1(request):
+    context_dict = {'myvar1': "Teszt valtozo"}
+    return render(request, 'rango/test1.html', context=context_dict)
